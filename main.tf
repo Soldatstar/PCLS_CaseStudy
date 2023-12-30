43b6a2541d89c3c06d643cf171f44c8419bca237
Damjan Mlinar <damjan.mlinar@students.fhnw.ch>
2023-12-30 09:28:08 +0100

    first nextcloud config

--- main.tf
+++ main.tf
@@ -1,10 +1,138 @@
-provider aws {
-  region = "eu-north-1"
-  access_key =  "AKIA2OFNDSDIPX3KA74D"
-  secret_key = "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"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-0c7217cdde317cfec"
-  instance_type = "t2.micro"
+# Configure the AWS Provider
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# Create aws vpc
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create aws internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+# Create aws ipv4 default route
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Subnet for servers
+resource "aws_subnet" "server-subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
+}
+
+# Subnet linking to route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.server-subnet.id
+  route_table_id = aws_route_table.route.id
+}
+
+# Security group for port 22,80,443
+resource "aws_security_group" "allow-web" {
+  name        = "allow-web"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "8080 from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "8443 from VPC"
+    from_port        = 8443
+    to_port          = 8443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+# Create network interface using subnet-server and security groups
+resource "aws_network_interface" "server-nic" {
+  subnet_id       = aws_subnet.server-subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow-web.id]
+}
+
+# Create public ip adress using aws elastic ip
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# Create first EC2 instance
+resource "aws_instance" "server" {
+  ami           = "ami-0014ce3e52359afbd"
+  instance_type = "t3.micro"
+  availability_zone = "eu-north-1a"
+  key_name = "pcls-sshkey"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.server-nic.id
+  }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install docker.io -y
+  sudo docker run --init --sig-proxy=false --name nextcloud-aio-mastercontainer --restart always --publish 80:80 --publish 8080:8080 --publish 8443:8443 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config --volume /var/run/docker.sock:/var/run/docker.sock:ro nextcloud/all-in-one:latest
+  EOF
 }
